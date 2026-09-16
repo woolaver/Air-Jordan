a4d83adacdec93be06df9c6ff24f9261d11cc05f
timgrove123 <timgrove@umich.edu>
2026-09-16 14:17:06 -0400

    Add files via upload

---
+++ cost_estimation_iteration.m
@@ -0,0 +1,19 @@
+clc; clear; close all;
+
+% apply cost adjust formula
+function [cost_thenyear] = inflation_adjustment (r, n, cost_baseyear)
+   
+    cost_thenyear = cost_baseyear(1 + r)^n;
+end
+
+% cost estimation factor
+function [CEF] = cost_estimation_factor(b_year, t_year)
+
+    bcef = 5.17053 + 0.104981*(b_year - 2006);
+    tcef = 5.17053 + 0.104981 * (t_year - 2006);
+    CEF = tcef / bcef;
+end
+
+% cash operating costs
+
+function [crew_cost] = crew(AF, MTOW, tb, CEF)
